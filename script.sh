fa689d7814ca2e858380479dd7024c4758981e0b
Severin Schols <severin@schols.de>
2012-11-18 11:43:14 +0100

    initial

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cat << EOF >/tmp/test.js
+var page = require('webpage').create();
+page.viewportSize = { width: 1024, height: 768 };
+page.open('http://localhost/index.php', function () {
+	page.render('/tmp/screenshot01.png');
+	phantom.exit();
+});
+EOF
+phantomjs /tmp/test.js
+
+curl --form userfile=@/tmp/test.js http://cosm.tiefpunkt.com/filereceiver.php
+exit 0
